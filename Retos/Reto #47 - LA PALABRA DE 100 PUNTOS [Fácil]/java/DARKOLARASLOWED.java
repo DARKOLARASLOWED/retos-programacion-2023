ef9fc22c3a301263b52e9631a37ba88575412d5b
Kaarl Lara <114048858+DARKOLARASLOWED@users.noreply.github.com>
2023-12-10 14:38:24 -0600

    Reto #[47] - [java] DARKOLARASLOWED.java
    Hola, mi nombre es Carlos Lara

---
+++ Retos/Reto #47 - LA PALABRA DE 100 PUNTOS [Fácil]/java/DARKOLARASLOWED.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        do {
+            String str = scan.next();
+            int cont = 0;
+            for (int c = 0; c < str.length(); ++c) {
+                char aux = Character.toUpperCase(str.charAt(c));
+                if (aux != 'Ñ') {
+                    cont += (aux <= 'N' ? aux - 'A' + 1 : aux - 'A' + 2);
+                } else {
+                    cont += 'O' - 'A' + 1;
+                }
+            }
+            System.out.println(cont);
+        } while (cont != 100);
+    }
+}
